9810173f14fa325cf4e39332578f69d3e3db133a
lrleon <leandro.r.leon@gmail.com>
2020-10-08 22:44:18 -0500

    Added go.mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module simple_sampler
+
+go 1.13
+
+require github.com/geniussportsgroup/treaps v1.0.2
--- sampler_test.go
+++ sampler_test.go
@@ -1,12 +1,18 @@
 package simple_sampler
 
 import (
-	"fmt"
-	"github.com/stretchr/testify/assert"
 	"math/rand"
 	"testing"
+	"time"
 )
 
 func TestSimpleSampler_append(t *testing.T) {
 
+	sampler := NewSampler(100, time.Minute, func(s1, s2 interface{}) bool {
+		return s1.(int) < s2.(int)
+	})
+
+	for i := 0; i < 100; i++ {
+		sampler.append(time.Now(), rand.Intn(300))
+	}
 }
